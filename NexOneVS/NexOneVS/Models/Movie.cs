5ba30b85a01537e47e09621e6b1f209797a7fafb
cqly <ccchanly@gmail.com>
2015-10-11 12:36:40 -0700

    added movie json for assignment 3

---
+++ NexOneVS/NexOneVS/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NexOneVS.Models
+{
+    public class Movie
+    {
+        public string Name { get; set; }
+        public double Rating { get; set; }
+        public Genre [] Genre { get; set; }
+        public int Year { get; set; }
+
+    }
+}
