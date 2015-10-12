69bc6dff9aa984467d900611167812e32b9d3094
Aliscad <knight_9_3@hotmail.com>
2015-10-11 21:50:49 -0700

    Created GamesController.cs and an function GetDetail returning a string

---
+++ NexOneVS/NexOneVS/Controllers/GamesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NexOneVS.Controllers
+{
+    public class GamesController : Controller
+    {
+        // GET: Games
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Please delete this when implementing this class.
+        //This is for homework purpose only
+        public string GetDetail()
+        {
+            return "wyan homework #3 api";
+        }
+    }
+}
