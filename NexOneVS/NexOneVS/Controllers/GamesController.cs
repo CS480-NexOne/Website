294a55d833243c13f87e8fca51edac34e6f8f585
Aliscad <knight_9_3@hotmail.com>
2015-11-03 22:39:09 -0800

    Pushing GameController

--- NexOneVS/NexOneVS/Controllers/GamesController.cs
+++ NexOneVS/NexOneVS/Controllers/GamesController.cs
@@ -1,6 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NexOneVS.Models.GameDB;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,17 +14,46 @@ namespace NexOneVS.Controllers
 {
     public class GamesController : Controller
     {
-        // GET: Games
+        //private string apikey = System.Configuration.ConfigurationManager.AppSettings["MovieDB_API_Key"].ToString();
+        private string apikey = "5195eec3fb1e59945f162c69ea935220a616fd95";
+
+        // GET: Movies
         public ActionResult Index()
         {
-            return View();
+            GameDB.Rootobject mygames = getNew();
+            //GameDB.Rootobject myGames = getNew();
+
+            return View(mygames);
         }
 
-        //Please delete this when implementing this class.
-        //This is for homework purpose only
-        public string GetDetail()
+        public GameDB.Rootobject getNew()
         {
-            return "wyan homework #3 api";
+            string url = "http://www.giantbomb.com/api/search/?api_key=5195eec3fb1e59945f162c69ea935220a616fd95&format=json&query=%22fallout%22&resources=game";
+
+            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+            HttpWebResponse response;
+
+            try
+            {
+                response = request.GetResponse() as HttpWebResponse;
+
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                    String json = reader.ReadToEnd();
+                    JObject jobj = (JObject)JsonConvert.DeserializeObject(json);
+
+                    GameDB.Rootobject gdb = new GameDB.Rootobject();
+
+                    gdb = JsonConvert.DeserializeObject<GameDB.Rootobject>(json);
+
+                    return gdb;
+                }
+            }
+            catch (WebException ex)
+            {
+                return null;
+            }
         }
     }
 }
