6ae47260686d75780e08f372c444daa4923099d6
Ara Malayan <aramalayan@gmail.com>
2015-12-04 21:02:55 -0800

    Working TV Page and Edited Formatting for MyList Buttons
    TV title display is now functioning.  TV specific values are displayed.
    TV shows can be added to the user's list.  MyList text formatting for
    buttons changed.

--- NexOneVS/NexOneVS/Models/TVDB/Title.cs
+++ NexOneVS/NexOneVS/Models/TVDB/Title.cs
@@ -8,29 +8,35 @@ namespace NexOneVS.Models.TV
 
     public class Title
     {
-        public bool adult { get; set; }
         public string backdrop_path { get; set; }
-        public object belongs_to_collection { get; set; }
-        public int budget { get; set; }
+        public Created_By[] created_by { get; set; }
+        public int[] episode_run_time { get; set; }
+        public string first_air_date { get; set; }
         public Genre[] genres { get; set; }
         public string homepage { get; set; }
         public int id { get; set; }
-        public string imdb_id { get; set; }
+        public bool in_production { get; set; }
+        public string[] languages { get; set; }
+        public string last_air_date { get; set; }
+        public string name { get; set; }
+        public Network[] networks { get; set; }
+        public int number_of_episodes { get; set; }
+        public int number_of_seasons { get; set; }
+        public string[] origin_country { get; set; }
         public string original_language { get; set; }
-        public string original_title { get; set; }
+        public string original_name { get; set; }
         public string overview { get; set; }
         public float popularity { get; set; }
         public string poster_path { get; set; }
         public Production_Companies[] production_companies { get; set; }
-        public Production_Countries[] production_countries { get; set; }
+        public Season[] seasons { get; set; }
         public string release_date { get; set; }
         public long revenue { get; set; }
         public int runtime { get; set; }
-        public Spoken_Languages[] spoken_languages { get; set; }
         public string status { get; set; }
         public string tagline { get; set; }
         public string title { get; set; }
-        public bool video { get; set; }
+        public string type { get; set; }
         public float vote_average { get; set; }
         public int vote_count { get; set; }
 
@@ -40,16 +46,26 @@ namespace NexOneVS.Models.TV
             public int id { get; set; }
         }
 
-        public class Production_Countries
+        public class Created_By
         {
-            public string iso_3166_1 { get; set; }
+            public int id { get; set; }
+            public string name { get; set; }
+            public string profile_path { get; set; }
+        }
+
+        public class Network
+        {
+            public int id { get; set; }
             public string name { get; set; }
         }
 
-        public class Spoken_Languages
+        public class Season
         {
-            public string iso_639_1 { get; set; }
-            public string name { get; set; }
+            public string air_date { get; set; }
+            public int episode_count { get; set; }
+            public int id { get; set; }
+            public string poster_path { get; set; }
+            public int season_number { get; set; }
         }
     }
 
